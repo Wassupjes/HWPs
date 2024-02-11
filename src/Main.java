e228078c4aadfd446dc0538b129f9844a4750607
Alexey <wassupjes@mail.ru>
2024-02-11 22:14:09 +0300

    Changed src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int ticketPrice = 25618;
         int bonusMiles = ticketPrice / 20;
+
         String m = "Начислено бонусных миль: ";
         System.out.println(m + bonusMiles);
-        }
     }
+}
